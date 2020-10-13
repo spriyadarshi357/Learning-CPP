c5651f05423480043193d6edcdc4ec60f5ad9495
Shubham Priyadarshi <72785560+spriyadarshi357@users.noreply.github.com>
2020-10-13 13:18:37 +0530

    Create 4-quotienet-and-remainder.cpp

---
+++ Basic/4-quotienet-and-remainder.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num = 94;
+	int divisor = 7;
+	
+	int q = num / divisor;
+
+	cout<<"Quotient : "<<q<<endl;
+	
+	int r = num % divisor;
+	
+	cout<<"Remainder : "<<r<<endl;
+	
+	return 0;
+}
