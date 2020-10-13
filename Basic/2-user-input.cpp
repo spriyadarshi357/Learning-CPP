35304a9dc84eaa992bdb83b6a9e3dd1b28e563c1
Shubham Priyadarshi <72785560+spriyadarshi357@users.noreply.github.com>
2020-10-13 13:04:27 +0530

    Create 2-user-input.cpp

---
+++ Basic/2-user-input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a;
+	cout<< "Enter the value of A : ";
+	cin>>a;
+	
+	cout<<"User selected "<<a<<" as value of A";
+	
+	return 0;
+}
