c1c88f4da80d2ba64cb0c1cefb1ecde77a3b4e69
Shubham Priyadarshi <72785560+spriyadarshi357@users.noreply.github.com>
2020-10-13 13:13:31 +0530

    Create 3-concat.cpp

---
+++ Basic/3-concat.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	string first_name = "Shubham";
+	string last_name = "Kumar";
+	
+	cout<<first_name<<" "<<last_name;
+	
+	return 0;
+}
